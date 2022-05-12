6f25211a857a657406b7f81109327b432a5baebc
Leandro Tabares Martín <ltmartin@uci.cu>
2022-05-12 12:18:37 +0200

    UPDATED: Some updates on comments and variable names.

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,6 +1,7 @@
 package base;
 
 import base.learners.QueryLearner;
+import base.services.MotifsService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,33 +27,34 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        clearConsole();
 
-        System.out.println("Welcome to SPARQLEAR demo!!!");
-        System.out.println("The format for the examples is: ");
-        System.out.println("+positiveExampleN -negativeExampleX");
-        System.out.println("If you want to type compound examples the format is: ");
-        System.out.println("+(component1, component2, componentN) -(component1, component2, componentN)");
-        System.out.println("Please, enter the examples:");
-
-        Scanner scanner = new Scanner(System.in, "utf-8");
-        while (scanner.hasNextLine()) {
-            String examples = scanner.nextLine();
-            System.out.println(examples);
-            // Piece of code to get a clean instance of the Spring bean
-            learner = learner.getLearner();
-
-            Optional<Set<String>> learnedQueries = learner.learn(examples);
-
-            System.out.println("===================================================");
-            System.out.println("Result: ");
-            if (!learnedQueries.isPresent())
-                System.out.println("Nothing learned.");
-            else
-                learnedQueries.stream().forEach(System.out::println);
-
-            System.out.println("===================================================");
-        }
+//        clearConsole();
+//
+//        System.out.println("Welcome to SPARQLEAR demo!!!");
+//        System.out.println("The format for the examples is: ");
+//        System.out.println("+positiveExampleN -negativeExampleX");
+//        System.out.println("If you want to type compound examples the format is: ");
+//        System.out.println("+(component1, component2, componentN) -(component1, component2, componentN)");
+//        System.out.println("Please, enter the examples:");
+//
+//        Scanner scanner = new Scanner(System.in, "utf-8");
+//        while (scanner.hasNextLine()) {
+//            String examples = scanner.nextLine();
+//            System.out.println(examples);
+//            // Piece of code to get a clean instance of the Spring bean
+//            learner = learner.getLearner();
+//
+//            Optional<Set<String>> learnedQueries = learner.learn(examples);
+//
+//            System.out.println("===================================================");
+//            System.out.println("Result: ");
+//            if (!learnedQueries.isPresent())
+//                System.out.println("Nothing learned.");
+//            else
+//                learnedQueries.stream().forEach(System.out::println);
+//
+//            System.out.println("===================================================");
+//        }
     }
 
     private void clearConsole(){
