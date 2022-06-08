de6eead799fcd1c3ab1f4734243b3b890cb638b4
Leandro Tabares Martín <ltmartin@uci.cu>
2022-06-08 13:53:10 +0200

    ADDED: A new test case for experiments.

---
+++ src/test/java/base/learners/QueryLearnerPerson2Test.java
@@ -0,0 +1,35 @@
+package base.learners;
+
+import base.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Optional;
+import java.util.Set;
+
+@SpringBootTest(classes = Application.class)
+public class QueryLearnerPerson2Test {
+
+    @Autowired
+    private QueryLearner queryLearner;
+
+    @Test
+    void peopleWhoKnowsPerson2AndPerson3() {
+        String examples = "+Person7 +Person10 -Person5 -Person8 -Person9 -Person4";
+        try {
+            Optional<Set<String>> learnedQueries = queryLearner.learn(examples);
+            System.out.println("===================================================");
+            System.out.println("Result: ");
+            if (!learnedQueries.isPresent())
+                System.out.println("Nothing learned.");
+            else
+                learnedQueries.stream().forEach(System.out::println);
+            System.out.println("===================================================");
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
